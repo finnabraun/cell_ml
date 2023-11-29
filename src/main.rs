9a60c48d8441a01cbd8bac303421d680aded7e09
finnabraun <finn.braun.ab@gmail.com>
2023-11-28 21:25:19 -0700

    renamed project, added to training loop:

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-//Global Model
-// 8x8 grid
-let mut cells[f64; 64] = [0; 64];
+use rand::prelude::*;
 
 fn main() {
+    //data
+    let mut cells = [0.0; 100];
+    //random number
+    let mut rng = rand::thread_rng();
+    
+    for _ in 0..10000 {
+        //random number [1,10]
+        let x1: f64 = rng.gen();
+        let x2: f64 = rng.gen();
+        let x1 = x1 * 10.0;
+        let x2 = x2 * 10.0;
+        //compute training datapoint
+        let val = x1 * x2;
+        //locate cell
+        let index = (x1 as u64) + (x2 as u64) * 10;
+    }
 
 }
-
-fn evaluate(coords: (f64, f64)) -> f64 {
-    let x_cell = coords.0 as u32;
-    let y_cell = coords.1.as u32;
-    let index: u32 = x_cell + 
